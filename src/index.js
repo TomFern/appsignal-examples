9e5ff8a4ad07ff9fa3c090e98d5810b572ea1329
Adam Yeats <adam@appsignal.com>
2019-10-05 00:42:33 +0200

    Load example from config and lint

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import Appsignal from '@appsignal/javascript';
 
 const appsignal = new Appsignal({
-  key: '<API KEY HERE>',
+  key: process.env['APPSIGNAL_FRONT_END_KEY'],
+  revision: process.env['APPSIGNAL_REVISION_SHA']
 });
 
 window.appsignal = appsignal
--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,38 @@
-const webpack = require('webpack');
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const { AppsignalPlugin } = require("@appsignal/webpack")
+const webpack = require("webpack");
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { AppsignalPlugin } = require("@appsignal/webpack");
+const Dotenv = require("dotenv-webpack");
+
+require("dotenv").config();
 
 const config = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    path: path.resolve(__dirname, "public"),
+    filename: "bundle.js"
   },
   module: {
     rules: [
       {
         test: /\.js$/,
-        use: 'babel-loader',
+        use: "babel-loader",
         exclude: /node_modules/
       }
     ]
   },
   plugins: [
+    new Dotenv(),
     new HtmlWebpackPlugin(),
     new AppsignalPlugin({
-      apiKey: "<YOUR PUSH API KEY>",
-      release: "<GIT COMMIT SHA>",
-      appName: "<APP NAME ON APPSIGNAL.COM>",
-      environment: "<CURRENT ENVIRONMENT>",
-      urlRoot: "<HOST THAT JS FILE WILL BE SERVED FROM>"
+      apiKey: process.env["APPSIGNAL_PUSH_API_KEY"],
+      release: process.env["APPSIGNAL_REVISION_SHA"],
+      appName: process.env["APPSIGNAL_APP_NAME"],
+      environment: process.env["APPSIGNAL_ENVIRONMENT"],
+      urlRoot: process.env["APPSIGNAL_URL_ROOT"]
     })
   ],
   devtool: "source-map"
-}
+};
 
 module.exports = config;
