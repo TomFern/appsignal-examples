886c3395c209098f5c5cd84632cc88e487f64ab9
tomas f <46322567+TomFern@users.noreply.github.com>
2020-08-03 19:10:07 -0300

    node.js tests

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const { appsignal } = require("./appsignal")
+
+// tom: try pg db
+const pgPlugin = require("@appsignal/pg");
+appsignal.instrument(pgPlugin);
+const Pool = require('pg').Pool
+const pool = new Pool({
+  user: 'postgres',
+  host: 'localhost',
+  database: 'postgres',
+  password: '',
+  port: 5432,
+});
+
+
 const { expressMiddleware, expressErrorHandler } = require("@appsignal/express")
 const createError = require("http-errors")
 const express = require("express")
 const path = require("path")
 const cookieParser = require("cookie-parser")
 const logger = require("morgan")
-
 const app = express()
+
+// tom
+// console.log(process.env)
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"))
@@ -17,6 +33,10 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(cookieParser())
 app.use(express.static(path.join(__dirname, "public")))
+
+// tom: slow everthing down
+// app.use(function(req,res,next){setTimeout(next,2000)});
+
 app.use(expressMiddleware(appsignal))
 
 /* GET home page. */
@@ -47,6 +67,87 @@ app.get("/", function(req, res) {
     response: "ok"
   })
 })
+
+// tom: metric test
+app.use("/metric-tests", function(req, res, next) {
+  const meter = appsignal.metrics();
+
+  meter.setGauge("myGauge", 200, { mytag: "myvalue"});
+  meter.addDistributionValue("myMeasurement", 10, { host: "A"});
+  meter.addDistributionValue("myMeasurement", 20, { host: "B"});
+
+  res.json({
+    metrics: "ok"
+  });
+
+});
+
+// tom: simulate slow process with a tiieout
+app.use('/slow', function(req, res, next) {
+   setTimeout(function() {
+     res.json({slow: "I take 3 seconds"});
+   }, 3000);
+});
+
+// tom: try pg long query
+const longQuery = (req, res) => {
+  const tracer = appsignal.tracer()
+  const span = tracer.currentSpan()
+  tracer.withSpan(span.child(), child => {
+    child
+      .setName("Slow postgres query example")
+      .setCategory("query.postgres")
+      .setSQL("SELECT pg_sleep(5)");
+
+    pool.query('select pg_sleep(5)', (err, data) => {
+      if(err) throw err;
+      res.status(200).json({query: "This should take 5 seconds."});
+      child.close();
+    });
+  });
+};
+
+app.use('/query', longQuery);
+
+// function(req, res, next) {
+//   const tracer = appsignal.tracer()
+//   const span = tracer.currentSpan()
+  
+  
+
+//     client.connect();
+//       // client.query('SELECT $1::text as message', ['Hello world!'], (err, data) => {
+//     client.query('SELECT pg_sleep(5)', (err, data) => {
+//       data = "I take 5 seconds.";
+//       // const data = data.rows[0].message;
+//       client.end();
+//       child.close();
+//       res.json({query: data});  
+//     });
+    
+    
+//   });
+// });
+
+// tom: does instrumentation affect how things are logged?
+app.use('/slow_instrumented', function(req, res, next) {
+
+  const tracer = appsignal.tracer();
+  const span = tracer.currentSpan();
+
+  tracer.withSpan(span.child(), child => {
+    child
+      .setName("Slow endpoint")
+      .setCategory("api.net_http");
+      // .setSQL("SELECT * FROM BLA WHERE user=1");
+
+    setTimeout(function() {
+      res.json({slow: "I take 2 seconds"});
+    }, 2000);
+
+    child.close();
+  });
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- appsignal.js
+++ appsignal.js
@@ -2,5 +2,5 @@ const { Appsignal } = require("@appsignal/nodejs")
 
 exports.appsignal = new Appsignal({
   active: true,
-  name: "Node.js Span Testing"
+  name: "Node.js Span Testing (default example)"
 })
